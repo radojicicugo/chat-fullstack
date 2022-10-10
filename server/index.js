fc9d195cdcc7d3fa29babca3a42de9b99cf39cf0
radojicicugo <74132264+radojicicugo@users.noreply.github.com>
2022-10-10 08:52:59 +0200

    first commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+
 app.use(cors());
 
 const server = http.createServer(app);
